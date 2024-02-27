0b37d355c04425a49d5c1b45159c3852eb56b8ae
Hoshizora Yuzu <hoshizorayuzu@gmail.com>
2024-02-27 06:03:09 -0800

    xxxxx

---
+++ scipt.js
@@ -0,0 +1,11 @@
+document.getElementById('navToggle').addEventListener('click', function() {
+    var navLinks = document.getElementById('navLinks');
+    if (navLinks.style.display === "block") {
+      navLinks.style.display = "none";
+    } else {
+      navLinks.style.display = "block";
+    }
+  });
+  
+  // Include any additional JavaScript code here
+  
